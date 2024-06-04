61c214da24674c3f3b067c588f4d326b227e2ba5
MKlimek00 <klimekmaciej00@gmail.com>
2024-06-05 00:32:10 +0200

    (docs): tests docstrings and annotations

--- test.py
+++ test.py
@@ -3,15 +3,24 @@ import utils
 
 class TestInput(unittest.TestCase):
     
-    def test_normalize_probabilities_correct_input(self):
+    def test_normalize_probabilities_correct_input(self) -> None:
+        """
+        Sprawdzenie podstawowego działania funkcji
+        """
         result = utils.normalize_probabilities([1,2,1])
         self.assertEqual(result, [0.25, 0.5, 0.25])
 
-    def test_normalize_probabilities_negative_number_in_input(self):
+    def test_normalize_probabilities_negative_number_in_input(self) -> None:
+        """
+        Sprawdzenie zabezpieczeń na wypadek negatywnych liczb w liście
+        """
         result = utils.normalize_probabilities([-3, -4, 3, 4, 1])
         self.assertEqual(result, [0.2, 0.2, 0.2, 0.2, 0.2])
 
-    def test_normalize_probabilities_all_zeros(self):
+    def test_normalize_probabilities_all_zeros(self) -> None:
+        """
+        Sprawdzenie zabezpieczeń na wypadek podania zer w liście
+        """
         result = utils.normalize_probabilities([0,0,0,0])
         self.assertEqual(result, [0.25, 0.25, 0.25, 0.25])
 
